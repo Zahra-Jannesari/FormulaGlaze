8aedfe1969398fcc2a2c77f03751ca4103395fe1
Zarisa <jannesari.zarisa@gmail.com>
2022-05-05 16:59:41 +0430

    fix adapter and submit testData in view model

--- app/src/main/java/com/zarisa/formulaglaze/vmodel/ViewModel.kt
+++ app/src/main/java/com/zarisa/formulaglaze/vmodel/ViewModel.kt
@@ -3,11 +3,18 @@ package com.zarisa.formulaglaze.vmodel
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
+import com.zarisa.formulaglaze.adapters.FormulaListAdapter
 
 class MainViewModel(app: Application) : AndroidViewModel(app) {
     var formulaCounterLiveData: LiveData<Int>
+    val formulaAdapter = FormulaListAdapter()
     init {
         Repository.initDB(app.applicationContext)
         formulaCounterLiveData=Repository.getCountOfFormulas()
+    }
+
+    fun listForFormulaList() {
+        Repository.addTestDate()
+        formulaAdapter.submitList(Repository.getFormulaList())
     }
 }
