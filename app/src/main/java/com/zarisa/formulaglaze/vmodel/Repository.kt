4baf084619c0097a3474b25b97e1b4075638a262
Zarisa <jannesari.zarisa@gmail.com>
2022-05-05 16:58:27 +0430

    add methods to add test data and get list of formulas in repository to active recyclerView

--- app/src/main/java/com/zarisa/formulaglaze/vmodel/Repository.kt
+++ app/src/main/java/com/zarisa/formulaglaze/vmodel/Repository.kt
@@ -3,7 +3,9 @@ package com.zarisa.formulaglaze.vmodel
 import android.content.Context
 import androidx.lifecycle.LiveData
 import com.zarisa.formulaglaze.database.AppDatabase
+import com.zarisa.formulaglaze.database.Formula
 import com.zarisa.formulaglaze.database.FormulaDao
+import com.zarisa.formulaglaze.model.Material
 
 object Repository {
     private lateinit var formulaDao: FormulaDao
@@ -13,5 +15,19 @@ object Repository {
 
     fun getCountOfFormulas(): LiveData<Int> {
         return formulaDao.getCountOfFormulas()
+    }
+    fun addTestDate() {
+        formulaDao.deleteAllFormulaList(formulaDao.getFormulaList())
+        for (i in 1..5)
+        formulaDao.insert(
+            Formula(
+                i, "blue$i", arrayListOf(
+                    Material("a$i", 1.2+i), Material("a${i+1}", 5.42+i)
+                )
+            )
+        )
+    }
+    fun getFormulaList():List<Formula>{
+        return formulaDao.getFormulaList()
     }
 }
