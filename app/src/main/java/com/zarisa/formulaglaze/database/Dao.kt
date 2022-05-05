c8aa1ea72af6572ba8b7bce547fac3defaec69ee
Zarisa <jannesari.zarisa@gmail.com>
2022-05-05 19:28:38 +0430

    add new query for searching among the name of formula

--- app/src/main/java/com/zarisa/formulaglaze/database/Dao.kt
+++ app/src/main/java/com/zarisa/formulaglaze/database/Dao.kt
@@ -5,6 +5,8 @@ import androidx.room.*
 
 @Dao
 interface FormulaDao{
+    @Query("SELECT * From Formula Where formulaName LIKE '%' || :text || '%'")
+    fun getMatches(text:String):List<Formula>
     @Query("SELECT * FROM Formula WHERE formulaName in (:wantedName) LIMIT 1")
     fun getFormulaByName(wantedName:String):Formula?
     @Query("SELECT * FROM Formula WHERE formulaId in (:wantedId)")
