ac0db642a281550bec90041c34768f1923cb52d0
Zarisa <jannesari.zarisa@gmail.com>
2022-05-05 15:31:31 +0430

    inflate binding and viewModel in formula fragment

--- app/src/main/java/com/zarisa/formulaglaze/FormulasFragment.kt
+++ app/src/main/java/com/zarisa/formulaglaze/FormulasFragment.kt
@@ -5,14 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import com.zarisa.formulaglaze.databinding.FragmentFormulasBinding
+import com.zarisa.formulaglaze.vmodel.MainViewModel
 
 
 class FormulasFragment : Fragment() {
+    lateinit var binding:FragmentFormulasBinding
+    private val viewModel: MainViewModel by viewModels()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_formulas, container, false)
+    ): View {
+        binding= FragmentFormulasBinding.inflate(layoutInflater, container, false)
+        return binding.root
     }
 }
