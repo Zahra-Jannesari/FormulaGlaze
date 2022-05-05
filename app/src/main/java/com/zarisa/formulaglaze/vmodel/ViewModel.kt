f1b934be4ba51ff10655e8aeb73d8d8c56026556
Zarisa <jannesari.zarisa@gmail.com>
2022-05-05 19:30:11 +0430

    refactor the view model for advance recycler and compatible for live search

--- app/src/main/java/com/zarisa/formulaglaze/vmodel/ViewModel.kt
+++ app/src/main/java/com/zarisa/formulaglaze/vmodel/ViewModel.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import com.zarisa.formulaglaze.adapters.FormulaListAdapter
+import com.zarisa.formulaglaze.database.Formula
 
 class MainViewModel(app: Application) : AndroidViewModel(app) {
     var formulaCounterLiveData: LiveData<Int>
@@ -15,6 +16,18 @@ class MainViewModel(app: Application) : AndroidViewModel(app) {
 
     fun listForFormulaList() {
         Repository.addTestDate()
-        formulaAdapter.submitList(Repository.getFormulaList())
+        submitFormulaList(Repository.getFormulaList())
+    }
+
+    fun filter(text: String) {
+        text.let {
+            if(it.isNullOrBlank())
+                submitFormulaList(Repository.getFormulaList())
+            else submitFormulaList(Repository.getSearchMatchList(it))
+        }
+
+    }
+    fun submitFormulaList(list:List<Formula>){
+        formulaAdapter.submitList(list)
     }
 }
