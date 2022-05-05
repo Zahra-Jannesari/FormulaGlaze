822d04a4210e48964049562407e748d0a41c4244
Zarisa <jannesari.zarisa@gmail.com>
2022-05-05 15:13:12 +0430

    add dao and main queries

---
+++ app/src/main/java/com/zarisa/formulaglaze/database/Dao.kt
@@ -0,0 +1,24 @@
+package com.zarisa.formulaglaze.database
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface WordDao{
+    @Query("SELECT * FROM Formula WHERE formulaName in (:wantedName) LIMIT 1")
+    fun getFormulaByName(wantedName:String):Formula?
+    @Query("SELECT * FROM Formula WHERE formulaId in (:wantedId)")
+    fun getFormulaById(wantedId:Int):Formula
+    @Query("SELECT count(*) FROM Formula")
+    fun getCountOfFormulas(): LiveData<Int>
+    @Query("SELECT count(*) FROM Formula")
+    fun formulaListSize():Int
+    @Query("SELECT * From formula")
+    fun getFormulaList():List<Formula>
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insert(formula: Formula)
+    @Update
+    fun updateWord(formula: Formula)
+    @Delete
+    fun deleteWord(formula: Formula)
+}
