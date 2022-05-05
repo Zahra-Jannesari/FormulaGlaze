3dba2805eff45671a0fbb8d4819e73ef4122bada
Zarisa <jannesari.zarisa@gmail.com>
2022-05-05 15:05:19 +0430

    add materialListConverter

---
+++ app/src/main/java/com/zarisa/formulaglaze/model/Converters.kt
@@ -0,0 +1,26 @@
+package com.zarisa.formulaglaze.model
+
+import androidx.room.TypeConverter
+
+class MaterialListConverter {
+    @TypeConverter
+    fun stringFromMaterialList(materials: List<Material>): String {
+        var materialListString =
+            "${materials[0].materialName},${materials[0].materialAmount},${materials[0].materialDescription}"
+        for (i in 1 until materials.size) {
+            materialListString += "/${materials[i].materialName},${materials[i].materialAmount},${materials[i].materialDescription}"
+        }
+        return materialListString
+    }
+
+    @TypeConverter
+    fun stringToMaterialList(materialListString: String): List<Material> {
+        var materialList = arrayListOf<Material>()
+        var materialStrings = materialListString.split('/')
+        for (str in materialStrings) {
+            var material = str.split(',')
+            materialList.add(Material(material[0], material[1].toDouble(),material[3]))
+        }
+        return materialList
+    }
+}
