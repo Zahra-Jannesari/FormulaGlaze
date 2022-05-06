0c8f92b40a697e026881ac0e16d21a0219c2adf8
Zarisa <jannesari.zarisa@gmail.com>
2022-05-06 09:30:25 +0430

    move adapting from viewModel to the fragment itself

--- app/src/main/java/com/zarisa/formulaglaze/FormulasFragment.kt
+++ app/src/main/java/com/zarisa/formulaglaze/FormulasFragment.kt
@@ -9,20 +9,23 @@ import androidx.core.os.bundleOf
 import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import com.zarisa.formulaglaze.adapters.FormulaListAdapter
 import com.zarisa.formulaglaze.database.Formula
 import com.zarisa.formulaglaze.databinding.FragmentFormulasBinding
 import com.zarisa.formulaglaze.vmodel.MainViewModel
+import com.zarisa.formulaglaze.vmodel.Repository
 import java.util.*
 
 
 class FormulasFragment : Fragment() {
-    lateinit var binding:FragmentFormulasBinding
+    lateinit var binding: FragmentFormulasBinding
     private val viewModel: MainViewModel by viewModels()
+    var formulAdapter = FormulaListAdapter()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding= FragmentFormulasBinding.inflate(layoutInflater, container, false)
+        binding = FragmentFormulasBinding.inflate(layoutInflater, container, false)
         return binding.root
     }
 
@@ -32,32 +35,37 @@ class FormulasFragment : Fragment() {
         adaptRecyclerView()
         onClicks()
     }
-
-    private fun onClicks() {
-        binding.editTextSearch.doOnTextChanged { inputText, _, _, _ ->
-            viewModel.filter(inputText.toString().lowercase(Locale.getDefault()))
-        }
-//        binding.textFieldSearch.setEndIconOnClickListener {
-//        }
-        binding.fabAdd.setOnClickListener {
-            val bundle = bundleOf(EDIT to false)
-            findNavController().navigate(R.id.action_formulasFragment_to_addOrEditFormuliaFragment,bundle)
-        }
-    }
-
-    private fun adaptRecyclerView() {
-        viewModel.listForFormulaList()
-        binding.recyclerview.adapter = viewModel.formulaAdapter
-    }
-
     private fun setObserver() {
         viewModel.formulaCounterLiveData.observe(viewLifecycleOwner) {
             binding.textViewCounter.text = it?.toString() ?: "0"
         }
     }
-
-    fun seeFormula(formula:Formula){
-        val bundle = bundleOf(EDIT to true, FormulaID to formula)
-        findNavController().navigate(R.id.action_formulasFragment_to_addOrEditFormuliaFragment,bundle)
+    private fun adaptRecyclerView() {
+        binding.recyclerview.adapter = formulAdapter
+        submitFormulaList(Repository.getFormulaList())
+    }
+    private fun onClicks() {
+        binding.editTextSearch.doOnTextChanged { inputText, _, _, _ ->
+            filter(inputText.toString().lowercase(Locale.getDefault()))
+        }
+//        binding.textFieldSearch.setEndIconOnClickListener {
+//        }
+        binding.fabAdd.setOnClickListener {
+            val bundle = bundleOf(EDIT to false)
+            findNavController().navigate(
+                R.id.action_formulasFragment_to_addOrEditFormuliaFragment,
+                bundle
+            )
+        }
+    }
+    private fun submitFormulaList(list: List<Formula>) {
+        formulAdapter.submitList(list)
+    }
+    private fun filter(text: String) {
+        text.let {
+            if (it.isNullOrBlank())
+                submitFormulaList(Repository.getFormulaList())
+            else submitFormulaList(Repository.getSearchMatchList(it))
+        }
     }
 }
--- app/src/main/java/com/zarisa/formulaglaze/vmodel/ViewModel.kt
+++ app/src/main/java/com/zarisa/formulaglaze/vmodel/ViewModel.kt
@@ -3,34 +3,15 @@ package com.zarisa.formulaglaze.vmodel
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
-import com.zarisa.formulaglaze.adapters.FormulaListAdapter
 import com.zarisa.formulaglaze.database.Formula
 
 class MainViewModel(app: Application) : AndroidViewModel(app) {
     var formulaCounterLiveData: LiveData<Int>
-    val formulaAdapter = FormulaListAdapter()
     init {
         Repository.initDB(app.applicationContext)
         formulaCounterLiveData=Repository.getCountOfFormulas()
-    }
-
-    fun listForFormulaList() {
         Repository.addTestDate()
-        submitFormulaList(Repository.getFormulaList())
     }
-
-    fun filter(text: String) {
-        text.let {
-            if(it.isNullOrBlank())
-                submitFormulaList(Repository.getFormulaList())
-            else submitFormulaList(Repository.getSearchMatchList(it))
-        }
-
-    }
-    private fun submitFormulaList(list:List<Formula>){
-        formulaAdapter.submitList(list)
-    }
-
     fun getFormulaById(id: Int): Formula {
         return Repository.getFormulaById(id)
     }
