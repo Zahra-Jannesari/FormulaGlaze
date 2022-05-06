21f08bb192479fcfb730b9d1b983e74f45946254
Zarisa <jannesari.zarisa@gmail.com>
2022-05-06 10:59:23 +0430

    add view for new material in code

--- app/src/main/java/com/zarisa/formulaglaze/AddOrEditFormuliaFragment.kt
+++ app/src/main/java/com/zarisa/formulaglaze/AddOrEditFormuliaFragment.kt
@@ -1,5 +1,6 @@
 package com.zarisa.formulaglaze
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -12,17 +13,18 @@ import com.zarisa.formulaglaze.model.Material
 import com.zarisa.formulaglaze.vmodel.MainViewModel
 import com.zarisa.formulaglaze.vmodel.Repository
 
-const val EDIT="is edit time?"
-const val FormulaID="formulaID"
+const val EDIT = "is edit time?"
+const val FormulaID = "formulaID"
+
 class AddOrEditFormulaFragment : Fragment() {
     lateinit var binding: FragmentAddOrEditFormulaBinding
     private val viewModel: MainViewModel by viewModels()
-    private lateinit var theFormula:Formula
+    private lateinit var theFormula: Formula
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding= FragmentAddOrEditFormulaBinding.inflate(layoutInflater, container, false)
+        binding = FragmentAddOrEditFormulaBinding.inflate(layoutInflater, container, false)
         return binding.root
     }
 
@@ -39,22 +41,25 @@ class AddOrEditFormulaFragment : Fragment() {
     }
 
     private fun addNewLine() {
+        val inflater= activity?.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val rowView:View=inflater.inflate(R.layout.material_item,null)
+        binding.linearLayoutParent.addView(rowView,binding.linearLayoutParent.childCount-1)
     }
 
     private fun onClicks() {
         binding.btnSaveChange.setOnClickListener {
             for (i in 10..15)
-            Repository.formulaDao.insert(
-            Formula(
-                i, "blue$i", arrayListOf(
-                    Material("a$i", 1.2+i), Material("a${i+1}", 5.42+i)
+                Repository.formulaDao.insert(
+                    Formula(
+                        i, "blue$i", arrayListOf(
+                            Material("a$i", 1.2 + i), Material("a${i + 1}", 5.42 + i)
+                        )
+                    )
                 )
-            )
-        ) }
+        }
+        binding.btnNewMaterial.setOnClickListener { addNewLine() }
     }
 
     private fun putDataForEdit() {
     }
-
-
 }
