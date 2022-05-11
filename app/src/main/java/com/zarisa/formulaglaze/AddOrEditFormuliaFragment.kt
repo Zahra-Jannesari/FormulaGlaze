cf6e49f8a85413fe7eef07a818c8c0ac08b90da3
Zarisa <jannesari.zarisa@gmail.com>
2022-05-11 15:54:51 +0430

    improve: remove add line part and get formula by name not by id

--- app/src/main/java/com/zarisa/formulaglaze/AddOrEditFormuliaFragment.kt
+++ app/src/main/java/com/zarisa/formulaglaze/AddOrEditFormuliaFragment.kt
@@ -1,24 +1,23 @@
 package com.zarisa.formulaglaze
 
-import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import com.zarisa.formulaglaze.adapters.MaterialListAdapter
 import com.zarisa.formulaglaze.database.Formula
 import com.zarisa.formulaglaze.databinding.FragmentAddOrEditFormulaBinding
-import com.zarisa.formulaglaze.model.Material
 import com.zarisa.formulaglaze.vmodel.MainViewModel
-import com.zarisa.formulaglaze.vmodel.Repository
 
 const val EDIT = "is edit time?"
-const val FormulaID = "formulaID"
+const val FormulaNAME = "formulaName"
 
 class AddOrEditFormulaFragment : Fragment() {
     lateinit var binding: FragmentAddOrEditFormulaBinding
     private val viewModel: MainViewModel by viewModels()
+    private val materialAdapter = MaterialListAdapter()
     private lateinit var theFormula: Formula
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -31,35 +30,20 @@ class AddOrEditFormulaFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         if (requireArguments().getBoolean(EDIT, false)) {
-            theFormula = viewModel.getFormulaById(requireArguments().getInt(FormulaID, 0))
+            theFormula = viewModel.getFormulaByName(requireArguments().getString(FormulaNAME, ""))
             putDataForEdit()
-        }
-        else{
-            addNewLine()
         }
         onClicks()
     }
 
-    private fun addNewLine() {
-        val inflater= activity?.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
-        val rowView:View=inflater.inflate(R.layout.material_item,null)
-        binding.linearLayoutParent.addView(rowView,binding.linearLayoutParent.childCount-1)
-    }
-
     private fun onClicks() {
         binding.btnSaveChange.setOnClickListener {
-            for (i in 10..15)
-                Repository.formulaDao.insert(
-                    Formula(
-                        i, "blue$i", arrayListOf(
-                            Material("a$i", 1.2 + i), Material("a${i + 1}", 5.42 + i)
-                        )
-                    )
-                )
         }
-        binding.btnNewMaterial.setOnClickListener { addNewLine() }
     }
 
     private fun putDataForEdit() {
+        binding.EditTextFormulaName.setText(theFormula.formulaName)
+        binding.recyclerview.adapter = materialAdapter
+        materialAdapter.submitList(theFormula.formulaMaterials)
     }
 }
