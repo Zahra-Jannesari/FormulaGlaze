34e262b7609be9ad7e66e607f3b6c624be445d7a
Zarisa <jannesari.zarisa@gmail.com>
2022-05-11 15:39:37 +0430

    improve: edit dao for new table

--- app/src/main/java/com/zarisa/formulaglaze/database/Dao.kt
+++ app/src/main/java/com/zarisa/formulaglaze/database/Dao.kt
@@ -4,25 +4,34 @@ import androidx.lifecycle.LiveData
 import androidx.room.*
 
 @Dao
-interface FormulaDao{
+interface FormulaDao {
     @Query("SELECT * From Formula Where formulaName LIKE '%' || :text || '%'")
-    fun getMatches(text:String):List<Formula>
-    @Query("SELECT * FROM Formula WHERE formulaId in (:wantedId)")
-    fun getFormulaById(wantedId:Int):Formula
+    fun getMatches(text: String): List<Formula>
+
+    @Query("SELECT * FROM Formula WHERE formulaName in (:wantedName)")
+    fun getFormulaByName(wantedName: String): Formula
+
     @Query("SELECT count(*) FROM Formula")
     fun getCountOfFormulas(): LiveData<Int>
+
     @Query("SELECT count(*) FROM Formula")
-    fun formulaListSize():Int
+    fun formulaListSize(): Int
+
     @Query("SELECT * From formula")
-    fun getFormulaListLivData():LiveData<List<Formula>>
+    fun getFormulaListLivData(): LiveData<List<Formula>>
+
     @Query("SELECT * From formula")
-    fun getFormulaList():List<Formula>
+    fun getFormulaList(): List<Formula>
+
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun insert(formula: Formula)
+    fun insert(vararg formula: Formula)
+
     @Update
     fun updateFormula(formula: Formula)
+
     @Delete
     fun deleteFormula(formula: Formula)
+
     @Delete
-    fun deleteAllFormulaList(list:List<Formula>)
+    fun deleteAllFormulaList(list: List<Formula>)
 }
